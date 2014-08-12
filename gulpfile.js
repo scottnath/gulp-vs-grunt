79f5b07b799c14cc8492c3384e73d0747480bad7
Scott Nath <github@scottnath.com>
2014-08-12 16:46:49 -0400

    functioning gulp version

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp      = require('gulp');
-var plumber   = require('gulp-plumber');
-var sass      = require('gulp-sass');
 var compass = require('gulp-compass');
 var notify = require('gulp-notify');
-var webserver = require('gulp-webserver');
-var opn       = require('opn');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -29,45 +25,23 @@ gulp.task('compass', function() {
     sass: 'app/scss'
   }))
   .pipe(gulp.dest(distPaths.styles)) // destination of sass files
-  .pipe(reload({stream:true}))
-  .pipe(notify('Compass run successfully')); // triggers browsersync to reload;
+  .pipe(reload({stream:true})) // triggers browsersync to reload
+  .pipe(notify('Compass run successfully')); 
 });
-
-// gulp.task('sass', function () {
-//   gulp.src( sourcePaths.styles )
-//     .pipe(plumber())
-//     .pipe(sass())
-//     .pipe(gulp.dest( distPaths.styles )) // destination of sass files
-//     .pipe(reload({stream:true})); // triggers browsersync to reload
-// });
-
-// gulp.task('webserver', function() {
-//   gulp.src( 'app' )
-//     .pipe(webserver({
-//       host:             server.host,
-//       port:             server.port,
-//       livereload:       true,
-//       directoryListing: false
-//     }));
-// });
 
 gulp.task('serve', ['compass'], function () {
-    browserSync.init(null, {
-        server: {
-            baseDir: 'app',
-            directory: true
-        },
-        debugInfo: false,
-        open: true,
-        host: server.host
-    }, function (err, bs) {
-        //require('opn')(bs.options.url);
-        console.log('Started connect web server on ' + bs.options.url);
-    });
-});
-
-gulp.task('openbrowser', function() {
-  opn( 'http://' + server.host + ':' + server.port );
+  browserSync.init(null, {
+    server: {
+      baseDir: 'app'
+    },
+    debugInfo: false,
+    open: true,
+    host: server.host,
+    port: server.port
+  }, function (err, bs) {
+    //require('opn')(bs.options.url);
+    console.log('Started connect web server on ' + server.host + ':' + server.port);
+  });
 });
 
 gulp.task('watch', function(){
@@ -76,4 +50,4 @@ gulp.task('watch', function(){
 
 gulp.task('build', ['compass']);
 
-gulp.task('default', ['build', 'serve', 'watch', 'openbrowser']);
+gulp.task('default', ['build', 'serve', 'watch']);
